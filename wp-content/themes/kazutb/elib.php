d864ea717941bf5658b710015cdf2faf22624818
Vladislav <akhmatov.vladislav@mail.ru>
2025-02-16 18:38:56 +0500

    Добавил перевод и добавление записей к станице E-Library

--- wp-content/themes/kazutb/elib.php
+++ wp-content/themes/kazutb/elib.php
@@ -6,45 +6,44 @@ get_header();
     <div class="container">
         <h2 class="elib-h2"><?= the_title() ?></h2>
         <div class="elib-wrapper">
-            <div class="elib-card">
-                <div class="elib-content">
-                    <p>Дата загрузки - 27.12.2024</p>
-                    <h3 class="elib-h3">Lorem ipsum odor</h3>
-                    <div class="elib-download">
-                        <a href="">
-                            <img src="./img/icon/download.svg" alt="download.png">
-                            <span>Скачать документ</span>
-                        </a>
+            <?php
+            $my_posts = get_posts(array(
+                'numberposts' => -1,
+                'category_name' => 'elib',
+                'orderby' => 'date',
+                'order' => 'DESC',
+                'include' => array(),
+                'exclude' => array(),
+                'meta_key' => '',
+                'meta_value' => '',
+                'post_type' => 'post',
+                'suppress_filters' => true,
+            ));
+
+
+            foreach ($my_posts as $item) {
+                setup_postdata($item);
+                ?>
+                <div class="elib-card">
+                    <div class="elib-content">
+                        <p><?= get_post_meta($post->ID, 'date_upload_text', true) ?>
+                            - <?= formatting_date($item->date_upload) ?></p>
+                        <h3 class="elib-h3"><?= $item->post_title ?></h3>
+                        <div class="elib-download">
+                            <a href="<?= wp_get_attachment_url($item->file) ?>" download>
+                                <img src="<?= get_template_directory_uri() . '/assets/img/icon/download.svg' ?>"
+                                    alt="download.png">
+                                <span><?= get_post_meta($post->ID, 'download_file', true) ?></span>
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="elib-card">
-                <div class="elib-content">
-                    <p>Дата загрузки - 27.12.2024</p>
-                    <h3 class="elib-h3">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-                    </h3>
-                    <div class="elib-download">
-                        <a href="">
-                            <img src="<?= get_template_directory_uri() . '/assets/img/icon/download.svg' ?>"
-                                alt="download.png">
-                            <span>Скачать документ</span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="elib-card">
-                <div class="elib-content">
-                    <p>Дата загрузки - 27.12.2024</p>
-                    <h3 class="elib-h3">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-                    </h3>
-                    <div class="elib-download">
-                        <a href="">
-                            <img src="./img/icon/download.svg" alt="download.png">
-                            <span>Скачать документ</span>
-                        </a>
-                    </div>
-                </div>
-            </div>
+                <?php
+
+            }
+
+            wp_reset_postdata(); // сброс
+            ?>
         </div>
     </div>
 </section>
